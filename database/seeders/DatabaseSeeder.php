62447a2e7a168cce58f6b5db32f6d54b0d93c46f
Roman <romaaverkin@gmail.com>
2022-08-12 14:41:00 +0700

    Сделал единый seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use App\Models\Contact;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,16 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            CompaniesTableSeeder::class,
-            ContactsTableSeeder::class,
-        ]);
-
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Company::factory()->count(10)->create();
+        Contact::factory()->count(50)->create();
     }
 }
